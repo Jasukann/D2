9b6168fc383c6075b3a774c6fa8078f71b088e16
Jasukann <137383402+Jasukann@users.noreply.github.com>
2023-08-24 12:38:28 +0500

    D2

---
+++ D2Moduls/NewsPaper/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Orders(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class Staff(models.Models):
+    pass
+
+
+class ProductOrder(models.Model):
+    pass
